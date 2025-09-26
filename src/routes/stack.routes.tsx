8bf5b7749c84f71099eac4e37ba6156de3338a9d
villarzz <hugovilar04@gmail.com>
2025-09-26 11:15:11 -0300

    feat: define RootStackParamList for type safety in navigation

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -2,13 +2,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../pages/Login';
 import Feed from '../pages/Feed';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  login: undefined;
+  feed: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function StackRoutes() {
-    return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name='login' component={Login}></Stack.Screen>
-            <Stack.Screen name='feed' component={Feed}></Stack.Screen>
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="login" component={Login} />
+      <Stack.Screen name="feed" component={Feed} />
+    </Stack.Navigator>
+  );
 }
